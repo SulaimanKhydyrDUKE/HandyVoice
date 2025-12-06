7a24d22030fe9df0e31b0f68be64fa1f96e801af
Sulaiman Khydyr uulu <sulaiman.khydyruulu@duke.edu>
2025-12-05 19:51:29 -0500

    Implement Viterbi algorithm for path finding

--- viterbi.rkt
+++ viterbi.rkt
@@ -1,46 +1,58 @@
-(define (deep-reverse x)
-  (cond [(null? x) '()]
-        [(pair? (car x))(append (deep-reverse (cdr x))(list (deep-reverse (car x))))]
-        [else(append (deep-reverse (cdr x))(list (car x)))]) )
+(define (viterbiPath emission switchPr s1 s2 p1 p2)
+  (letrec
+      ([step
+        (lambda (EM prev)
+          ;; prev = ((s1 prob1 path1) (s2 prob2 path2))
+          (let* ((x (car EM))
+                 (is_inverse (= x 1))
+                 (emit1 (if is_inverse (- 1 p1) p1))
+                 (emit2 (if is_inverse (- 1 p2) p2)))
 
+            (let* ((prev1 (car prev))
+                   ; (s1 prob path)
+                   (prev2 (cadr prev))
+                   (p1_prev (cadr prev1))
+                   (p2_prev (cadr prev2))
+                   (path1_prev (caddr prev1))
+                   (path2_prev (caddr prev2))
+                   (stay1 (* p1_prev (- 1 switchPr)))
+                   (switch2 (* p2_prev switchPr))
+                   (stay2 (* p2_prev (- 1 switchPr)))
+                   (switch1 (* p1_prev switchPr))
+                   
+                   (new1_raw-stay (* stay1 emit1))
+                   (new1_raw-switch (* switch2 emit1))
+                   (new2_raw-stay (* stay2 emit2))
+                   (new2_raw-switch (* switch1 emit2))
+                   (new1 (if (> new1_raw-stay new1_raw-switch)
+                             (list s1 new1_raw-stay (append path1_prev (list s1)
+                                                            )
+                                   )
+                             (list s1 new1_raw-switch (append path2_prev (list s1)))
+                             ))
 
-(deep-reverse '())
-()
-(reverse '(1 (2 3) 4 5))
-(deep-reverse '(1 (2 3) 4 5))
-(reverse '(1 (2 3) (4 (5 6) 7) (8 9 (10 11 (12)))))
-(deep-reverse '(1 (2 3) (4 (5 6) 7) (8 9 (10 11 (12)))))
+                   (new2 (if (> new2_raw-stay new2_raw-switch)
+                             (list s2 new2_raw-stay (append path2_prev (list s2)))
+                             (list s2 new2_raw-switch (append path1_prev (list s2))))))
 
+              (list new1 new2))))]
 
-(define viterbiProb
-  (emission k switchPr p1 p2)
-  (cond[(null? (cdr emission))
-        1]
-       [(
-         (let ((two_pairs (loop (cdr emission) (- k 1) switchPr p1 p2)) ;gets two pairs of F or B paired with their probabilities
-               (most_likely (max(two_pairs))) ;gets the most likely last coin 
-               (second_likely (if (= most_likely (car two_pairs) (car (cdr two_pairs)) (car two_pairs)))) ;gets second likely coin with its probability
-               (is_ml_p1 (if ( = (car most_likely) p1) #t #f))  ;Is Most likely p1 or not?
-               (is_inverse (if ( = (car emission) 1) #f #t)) ; is it 0 or not
-               (c_p1 (- 1 p1))
-               (c_p2 (- 1 p2))) ; return the complement of the probability
-         (cons 
-               ( (if (is_ml_p1)
-                     (pair p1 (car (cdr ((max
-                                          (
-                                           (* (car (cdr second_likely)) switchPr (if (is_inverse) c_p2 p2)) ; V(B) x switching x (inverse or not of p2, depending on the emission)
-                                           (* (car (cdr most_likely)) (- 1 switchPr) (if (is_inverse) c_p1 p1)) ; V(F) x not_switching x (inverse or not of p1, d on em)
-                                           )
+       ;; BASE CASE
+       [init
+        (let ((x (car emission)))
+          (let ((is_inverse (= x 1)))
+            (list
+             (list s1 (* 0.5 (if is_inverse (- 1 p1) p1)) (list s1))
+             (list s2 (* 0.5 (if is_inverse (- 1 p2) p2)) (list s2)))))])
 
-                                          )))))
-                     (pair p1 (car (cdr ((max(
-                                              (* (car (cdr second_likely)) (- 1 switchPr) (if (is_inverse) c_p1 p1)) ; V(F) x not_switching x (inverse or not of p1, d on em)
-                                              (* (car (cdr most_likely)) switchPr  (if (is_inverse) c_p2 p2))))))))) ;V(B) x switching x (inverse or not of p2, depending on the emission)
-                     ))
-                 ;;Where do I pass on the inverse probabilites? FUUUU
-               (pair (p2 ( max(* ())
-                             (* () ) ) ))))))
-         )]
-;Max of (Viterbi(k-1, p1)*switchPr*p1,
-   ;     Vitervi(k-1, p2)*(1-switchPr)*p1)
-   ))
+    ;; RC
+    (let loop ((EM (cdr emission))
+               (prev init))
+      (if (null? EM)
+          ;; choose best final one
+          (let* ((p1-prev (cadr (car prev)))
+                 (p2-prev (cadr (cadr prev))))
+            (if (> p1-prev p2-prev)
+                (list (caddr (car prev)) p1-prev)
+                (list (caddr (cadr prev)) p2-prev)))
+          (loop (cdr EM) (step EM prev))))))
