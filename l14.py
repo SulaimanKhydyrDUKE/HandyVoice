966e60fc900d938db2f5a26a6f1ae9950daf2088
Sulaiman Khydyr uulu <sulaiman.khydyruulu@duke.edu>
2025-12-10 12:42:39 -0500

    Optimize longestCommonPrefix algorithm
    Refactor longestCommonPrefix method to improve efficiency.

--- l14.py
+++ l14.py
@@ -32,3 +32,24 @@ class Solution(object):
                     ret = temp
         return ret
 # 2% faster
+# THE ONE BELOW BEATS 100% of all algorithms: 100% in runtime and 26% in memory
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        ret = strs[0]
+        for i in strs:
+            if len(i)<len(ret):
+                ret = i
+        print("RET: " + ret)
+        for i in range(len(strs)):
+            # so one mistak was not reading the first one as I expected ret to be set to first
+            if i<len(strs):
+                temp = ""
+                for k in range (min(len(ret), len(strs[i]))):
+                    if ret[k] == strs[i][k]:
+                        temp+=ret[k]
+                    else:
+                        break
+                if len(temp)<len(ret):
+                    ret = temp
+
+        return ret
