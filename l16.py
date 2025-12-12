993703a18d25fd7f7f4b0a5244d2050aa446ad8d
Sulaiman Khydyr uulu <sulaiman.khydyruulu@duke.edu>
2025-12-11 23:55:53 -0500

    Add threeSumClosest method for closest sum calculation
    Implement threeSumClosest function to find closest sum to target.

--- l16.py
+++ l16.py
@@ -31,3 +31,55 @@ class Solution(object):
         return close
             
 
+# WORKING SOLUTION FOR O(N^2) time that beats %5 in that regards, and 24% in memory
+class Solution(object):
+    def threeSumClosest(self, nums, target):
+        # 1) Sort it
+        nums.sort()
+      
+        # 2) Two pointers 
+        ret = [] 
+        ret_sum = float('inf')
+        ret_dist = float('inf')
+        for i, n in enumerate(nums):    
+            l, r = i+1, len(nums) -1
+            neg = False
+            
+            if target < 0:
+                neg = True
+            while l<r and r<len(nums):
+                
+                sum = n + nums[l]+nums[r]
+                if sum == target:
+                    return sum
+                dist = float('inf')
+                if neg:
+                    if sum<0:
+                        
+                        dist = abs(abs(target)-abs(sum))
+                    else:
+                        dist = abs(target) + sum
+                else:
+                    if sum<0:
+
+                        dist = abs(sum) + target
+           
+                    else:
+                        dist = abs(target-sum)
+                #if distance is smaller than the previous one, then replace one with another. 
+                if ret_dist>dist:
+                    
+                    ret_dist = dist
+                    ret_sum = sum
+                if sum<target:
+                    l+=1
+                elif sum>target:
+                    r-=1
+                else:
+                    return sum
+        return ret_sum
+
+                
+
+            
+
