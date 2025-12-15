a0ecc67980316487773636e659bbf7d540632706
Sulaiman Khydyr uulu <sulaiman.khydyruulu@duke.edu>
2025-12-14 23:50:13 -0500

    Implement prefix_reduce function for array reduction
    Added a function to reduce an array by prefix values.

--- EBayOA.py
+++ EBayOA.py
@@ -1,4 +1,5 @@
 #from my assessment at eBay
+#State Changes
 def count_state_changes(arr):
     if not arr:
         return 0
@@ -9,3 +10,29 @@ def count_state_changes(arr):
             changes += 1
 
     return changes
+#I got this question by 300/300
+
+
+# Find the leftmost non-zero value, substract from all values until you find one less than the non-zero value, then add the non-zero to the return value, repeat until the array is equal to 0.  Return the ret value
+def prefix_reduce(nums):
+    nums = nums[:]  # copy
+    ret = 0
+    n = len(nums)
+
+    while True:
+        i = 0
+        while i < n and nums[i] == 0:
+            i += 1
+        if i == n:
+            break
+
+        x = nums[i]
+        ret += x
+
+        j = i
+        while j < n and nums[j] >= x:
+            nums[j] -= x
+            j += 1
+
+    return ret
+    #I got this question 160/300
